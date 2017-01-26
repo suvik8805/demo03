b3626f108562bfd5286865a97252bfddc785846c
K8805 <K8805@OHJE-330-WS-37>
2017-01-26 14:34:34 +0200

    päivitys

---
+++ demo03/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo03
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        // default constructor
+        public Car()
+        {
+
+        }
+
+        // constructor...
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        // method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10; // arvo määritelty valmiiksi
+        }
+
+        // method to slow down
+        public void Brake(int value) // pääohjelma antaa arvon
+        {
+            Speed -= value; // Speed= Speed - value; 
+        }
+
+        // method to display car data/properties
+        public void PrintData()
+        {
+            Console.WriteLine("Car properties:");
+            Console.WriteLine(" - model :" + Model);
+            Console.WriteLine(" - color :" + Color);
+            Console.WriteLine(" - engine :" + Engine);
+            Console.WriteLine(" - speed :" + Speed);
+            Console.WriteLine(" - dices :" + FuzzyDices);
+            Console.WriteLine(" - door count :" + DoorCount);
+            Console.WriteLine(" - some static property :" + SomeProperty);
+        }
+
+        //destructor
+        ~Car()
+        {
+            Console.WriteLine("Car object destroyed!");
+
+        }
+
+    } // class
+} // namespace
--- demo03/Program.cs
+++ demo03/Program.cs
@@ -10,6 +10,29 @@ namespace demo03
     {
         static void Main(string[] args)
         {
+            // create a new instance from Car class
+            Car car = new Car();
+            car.PrintData();
+            // ominaisuudet car-oliolle
+            car.Model = "Datsun 100A";
+            car.Color = "Yellow";
+            car.Engine = 0.5;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            car.Accelerate();
+            car.PrintData();
+            car.Brake(7);
+            Car.SomeProperty = 56;
+            car.PrintData();
+
+            // create another car object
+            string model = "Speedster";
+            Car nascar = new Car(model);
+            nascar.PrintData();
+            nascar.Color = "Black";
+            Console.WriteLine("Color is " + nascar.Color);
+
         }
     }
 }
