1ca9dced3f22ed5d98c7c727d0220d8f62e13bbb
suvik8805 <suvik8805@users.noreply.github.com>
2017-02-02 12:34:37 +0200

    Update Program.cs

--- teht04_vehicle/Program.cs
+++ teht04_vehicle/Program.cs
@@ -14,19 +14,19 @@ namespace teht04_vehicle
             vehicle.Name = "Car";
             vehicle.Speed = 90;
             vehicle.Tyres = 4;
-            vehicle.PrintData();
+            vehicle.ToString();
 
             Vehicle vehicle2 = new Vehicle();
             vehicle2.Name = "Bike";
             vehicle2.Speed = 20;
             vehicle2.Tyres = 2;
-            vehicle2.PrintData();
+            vehicle2.Tostring();
 
             Vehicle vehicle3 = new Vehicle();
             vehicle3.Name = "Tractor";
             vehicle3.Speed = 40;
             vehicle3.Tyres = 4;
-            vehicle3.PrintData();
+            vehicle3.ToString();
 
 
         }
