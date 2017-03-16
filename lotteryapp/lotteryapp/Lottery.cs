3a566ae52cd5bc8b19658cac3ee87caabbda8e1f
suvij <suvij@DESKTOP-CC51IDH>
2017-03-16 11:08:00 +0200

    teht demo07

---
+++ lotteryapp/lotteryapp/Lottery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotteryapp
+{
+    class Lottery
+    {
+        public void Draw(){
+
+            Random rand = new Random();
+            int number = rand.Next(1, 39 + 1);
+            List<int> list = new List<int>();
+            list.Add(number);
+            
+        }
+    }
+}
---
+++ lotteryapp/lotteryapp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace lotteryapp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+            bool numbers = true;
+            int rows;
+            int i, j;
+            
+
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            Lottery lottery = new Lottery();
+            if (!int.TryParse(RowsTextbox.Text, out rows))
+            {
+                numbers = false;
+            }
+
+            ResultBlock.Text = "Random numbers:";
+            //Kaikkien lottorivien arvonta
+            for (j = 0; j < rows; j++)
+            {
+
+                // Yhden lottorivin arvonta
+                for (i = 0; i < 7; i++)
+                {
+                    lottery.Draw();
+                    ResultBlock.Text = "Row: " + list.Count;
+                }
+            }
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+            rows = 0;
+            ResultBlock.Text = " ";
+
+
+        }
+    }
+
+
